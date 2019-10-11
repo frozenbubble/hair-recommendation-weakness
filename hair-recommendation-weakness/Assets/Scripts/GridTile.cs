74e269c8282578c16de3228751acd4d81bd8788c
borgi <borgihun@gmail.com>
2019-10-11 15:40:16 +0200

    grides scene

---
+++ hair-recommendation-weakness/Assets/Scripts/GridTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile : MonoBehaviour
+{
+	
+	public enum GridMode {
+		Empty,
+		Hair,
+		Count
+	
+	}
+	
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ hair-recommendation-weakness/Assets/Scripts/HajGrid.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HajGrid : MonoBehaviour
+{
+	
+	public int x_size = 2;
+	public int y_size = 2;
+	
+	public float grid_tile_size = 1.1f;
+	
+	public GameObject BottomLeft;
+	public GameObject TopRight;
+	
+	public GameObject Fejbub;
+	
+	
+	public GameObject[,] Grids;
+	
+	
+	public float fromXAngle = 0;
+	public float toXAngle = 180;
+	
+	public float fromYAngle = 0;
+	public float toYAngle = 180;
+	
+	public float R = 3f;
+	
+	
+	public float TilesScale = 0.8f;
+	
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		SetupGrid();
+	}
+	
+	
+	
+	void SetupGrid(){
+	
+		if(Grids == null){
+			Grids = new GameObject[x_size + 1,y_size + 1];
+		}
+		
+		var xAngleStep = (toXAngle - fromXAngle) / (x_size);
+		var yAngleStep = (toYAngle - fromYAngle) / (y_size);
+		
+		int arri = 0;
+		int arrj = 0;
+		
+		for(float i = fromXAngle; i <= toXAngle; i += xAngleStep){
+			for(float j = fromYAngle; j <= toYAngle; j += yAngleStep){
+				
+				var spherePos = new Vector3(
+					R*Mathf.Sin(i * Mathf.Deg2Rad)*Mathf.Cos(j* Mathf.Deg2Rad),
+					R*Mathf.Sin(i* Mathf.Deg2Rad)*Mathf.Sin(j* Mathf.Deg2Rad),
+					R*Mathf.Cos(i* Mathf.Deg2Rad)
+				);
+				
+				GameObject instance = Instantiate(Resources.Load("Prefabs/HajGridTile", typeof(GameObject))) as GameObject;
+				instance.transform.parent = transform;
+				instance.transform.position= transform.position + spherePos;
+				instance.transform.localScale = new Vector3(TilesScale * instance.transform.localScale.x, 
+				                                            TilesScale * instance.transform.localScale.y,
+				                                            TilesScale * instance.transform.localScale.z);
+				
+				instance.transform.rotation = Quaternion.FromToRotation (transform.up, spherePos.normalized) * instance.transform.rotation;
+			
+				Grids[arri, arrj] = instance;
+				arrj++;
+			}
+			arrj = 0;
+			arri++;
+		}
+	}
+	
+	
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
